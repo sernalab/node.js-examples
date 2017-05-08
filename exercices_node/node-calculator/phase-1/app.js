3d06da1ce4c61bac8122f3c927f152275c5239ea
Carles Serna <carles.serna@gmail.com>
2017-05-08 16:33:30 +0200

    first exercise

---
+++ exercices_node/node-calculator/phase-1/app.js
@@ -0,0 +1,5 @@
+var sum = require('./calculator.js').sum
+var multi = require('./calculator.js').multi
+
+sum(3,7)
+multi(3,7)
---
+++ exercices_node/node-calculator/phase-1/calculator.js
@@ -0,0 +1,7 @@
+module.exports.sum = function sum(num1, num2) { 
+  console.log('The sum of 3 & 7 is: ' + (num1 + num2))
+}
+
+module.exports.multi = function multi(num1, num2) { 
+  console.log('The multiplication of 3 & 7 is: ' + (num1 * num2))
+}
